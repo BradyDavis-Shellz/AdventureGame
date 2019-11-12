a23b7645810e951f9f22620b3b44bd1f854bc767
bradydavis.ver.2.0 <bradydavis.ver.2.0@gmail.com>
2019-11-11 18:53:03 -0600

    adding condition reaction test

---
+++ Assets/Tests/ScriptableObjects/Interaction/Reactions/ImmediateReactions/ConditionReactionTests.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+[TestFixture]
+public class ConditionReactionTests
+{
+    ConditionReaction sut;
+    Condition condition;
+
+    [SetUp]
+    public void SetUp()
+    {
+        sut = new ConditionReaction();
+        Condition condition = new Condition();
+        condition.satisfied = false;
+        sut.condition = condition;
+        sut.satisfied = true;
+    }
+
+    [Test]
+    public void TestThatImmediateReactionSetsConditionSatisfiedToConditionReactionSatisfied()
+    {
+        sut.React(null);
+
+        Assert.AreEqual(sut.condition.satisfied, sut.satisfied);
+    }
+}
